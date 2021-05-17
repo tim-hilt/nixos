28b066615697f9d8fb67bba1cedbab8aaf6b4df3
Tim Hilt <timhilt@live.de>
2021-05-17 07:15:37 +0200

    Switch to latest kernel

--- config/default.nix
+++ config/default.nix
@@ -2,6 +2,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = [ "ntfs" ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.networkmanager.enable = true;
 
