8d9cc9252691f5ff699acb440c9a0c9ae47b1c28
Tim Hilt <thilt@in-technology.eu>
2021-06-01 17:13:54 +0200

    main: Added keybindings.json

--- home-manager/desktop/vscode/default.nix
+++ home-manager/desktop/vscode/default.nix
@@ -19,7 +19,7 @@
         name = "auto-comment-blocks";
         publisher = "kevinkyang";
         version = "1.0.1";
-        sha256 = "0f5n0i337h8bc66zv7j54rl42na09jpcivhs5s5f59g4vavrhfk7";
+        sha256 = "sha256-KwS69LhhmT/6J35JOsxvScUMGkYptAt1VofPem3qwA0=";
       }
     ];
   };
