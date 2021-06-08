fb71d3976435799271dc784ca90ac18b9f11048e
Tim Hilt <thilt@in-technology.eu>
2021-06-08 17:40:33 +0200

    main: Added vscode-thunder-client to work-config

--- home-manager/work/default.nix
+++ home-manager/work/default.nix
@@ -15,9 +15,16 @@
     extraConfig.core.hooksPath = toString ./git-hooks;
   };
 
-  programs.vscode.extensions = with pkgs.vscode-extensions; [
+  programs.vscode.extensions = (with pkgs.vscode-extensions; [
     golang.Go
     github.vscode-pull-request-github
+  ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    {
+      name = "vscode-thunder-client";
+      publisher = "rangav";
+      version = "1.4.1";
+      sha256 = "sha256-31OGOtjcrBQgbGIDojtnsAnLLa/EsQprBMTTtngzejI=";
+    }
   ];
 
   programs.go = {
