347b767251ad15e47eaf8b9f76a5be77ec99445c
Tim Hilt <thilt@in-technology.eu>
2021-06-09 18:15:03 +0200

    main: Added any-nix-shell

--- home-manager/server/default.nix
+++ home-manager/server/default.nix
@@ -1,34 +1,32 @@
 { pkgs, ... }: {
   imports = [ ./nvim ];
 
-  home.packages = with pkgs; [ fd ripgrep highlight nixfmt bat ];
+  home.packages = with pkgs; [ fd ripgrep highlight nixfmt bat any-nix-shell ];
 
   programs.fish = {
     enable = true;
-    functions = {
-      fish_greeting = {
-        body = "";
-      };
-    };
+    functions = { fish_greeting = { body = ""; }; };
     shellAbbrs = {
       k = "kill (ps aux | fzf | awk '{print $2}')";
       r = "ranger";
       nrs = "sudo nixos-rebuild switch";
       e = "nvim (fd -t f | fzf --preview='bat --color=always {}')";
       ni = "nmcli device wifi list";
-      iso_usb = "sudo dd bs=4M if=path/to/input.iso of=/dev/sd<?> conv=fdatasync status=progress";
+      iso_usb =
+        "sudo dd bs=4M if=path/to/input.iso of=/dev/sd<?> conv=fdatasync status=progress";
     };
-    plugins = [
-      {
-        name = "park.fish";
-        src = pkgs.fetchFromGitHub {
-          owner = "mattgreen";
-          repo = "park.fish";
-          rev = "main";
-          sha256 = "sha256-oJEmSez26xANGUlbTvMSfuv24/VpF18mzFi+NrWg55Q=";
-        };
-      }
-    ];
+    plugins = [{
+      name = "park.fish";
+      src = pkgs.fetchFromGitHub {
+        owner = "mattgreen";
+        repo = "park.fish";
+        rev = "main";
+        sha256 = "sha256-oJEmSez26xANGUlbTvMSfuv24/VpF18mzFi+NrWg55Q=";
+      };
+    }];
+    programs.fish.promptInit = ''
+      any-nix-shell fish --info-right | source
+    '';
   };
 
   programs.git = {
